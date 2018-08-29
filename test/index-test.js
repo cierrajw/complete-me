6fb1d7e566970c9bbffd4f9ebc6d94e8dd14ffc4
Cierra Washington <cierrajwashi@gmail.com>
2018-08-29 09:38:11 -0600

    init cmt

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,27 @@
  import { assert } from 'chai';
+ import Trie from '../lib/Trie';
     
  describe('test', function() {
    it('should return true', function() {
      assert.equal(true, true);
+   });
+
+   it('should be able to take in a word', function(){
+    let trie = new Trie(word);
+
+   });
+
+   it('should be able to add/insert a word', function(){
+    let trie = new Trie(word);
+
+    trie.insert(word);
+
+   });
+
+   it('should be able to keep count of added words', function(){
+    let trie = new Trie(word);
+
+    trie.insert(word);
+
    });
  });
